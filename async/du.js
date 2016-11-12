6a1b29b48386c9cf538d43db081f7748e7892477
David Herron <david@davidherron.com>
2016-11-12 14:18:59 -0800

    Update async examples

--- async/du.js
+++ async/du.js
@@ -4,14 +4,20 @@ const async = require('async');
 const fs    = require('fs');
 const util  = require('util');
 
-async.reduce(process.argv.slice(2), 0,
-(memo, filenm, next) => {
-    fs.stat(filenm, (err, stats) => {
-        if (err) next(err);
-        else next(undefined, stats.size + memo);
+var du = function(filez, fini) {
+    async.reduce(filez, 0, (memo, filenm, next) => {
+        fs.stat(filenm, (err, stats) => {
+            if (err) next(err);
+            else next(undefined, stats.size + memo);
+        });
+    },
+    (err, result) => {
+        if (err) fini(err);
+        else fini(undefined, result);
     });
-},
-(err, result) => {
+};
+
+du(process.argv.slice(2), (err, result) => {
     if (err) console.error(err.stack);
     else console.log(result);
 });
--- async/stats.js
+++ async/stats.js
@@ -5,7 +5,14 @@ const async = require('async');
 const fs    = require('fs');
 const util  = require('util');
 
-async.map(process.argv.slice(2), fs.stat, (err, results) => {
+var stats = function(filez, fini) {
+    async.map(filez, fs.stat, (err, results) => {
+        if (err) fini(err);
+        else fini(undefined, results);
+    });
+}
+
+stats(process.argv.slice(2), (err, results) => {
     if (err) console.error(err.stack);
     else {
         results.forEach(result => {
