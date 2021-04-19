e75bfdc3ea473cdb45218897a3e80848ee7d9bf9
David Herron <david@davidherron.com>
2021-04-19 13:34:10 -0700

    Add callback example

--- hybrid-es6-cjs-module/index.js
+++ hybrid-es6-cjs-module/index.js
@@ -8,11 +8,32 @@ async function hello() {
 }
 module.exports.hello = hello;
 
+function helloCB(done) {
+    aasync.then(m => {
+        m.hello()
+        .then(msg => { done(undefined, msg); })
+        .catch(err => { done(err); });
+    })
+    .catch(err => {
+        done(err);
+    });
+}
+module.exports.helloCB = helloCB;
+
 async function callHello() {
     let h = await module.exports.hello();
     console.log(h);
 }
 
 callHello()
-.then(() => { console.log('SUCCESS'); console.log(aasync); })
-.catch(err => { console.log('ERROR ', err); })
+.then(() => { console.log('callHello SUCCESS'); console.log(aasync); })
+.catch(err => { console.log('callHello ERROR ', err); })
+
+helloCB((err, msg) => {
+    if (err) {
+        console.log('helloCB ERROR ', err);
+    } else {
+        console.log('helloCB SUCCESS ', msg);
+        console.log(aasync);
+    }
+});
